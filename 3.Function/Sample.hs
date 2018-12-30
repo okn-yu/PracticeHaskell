c78163f0b3ae4481a8523337fa59f31ae0eece4d
okn-yu <37620738+okn-yu@users.noreply.github.com>
2018-12-30 12:09:48 +0900

    Update Sample.hs

--- 3.Function/Sample.hs
+++ 3.Function/Sample.hs
@@ -1,6 +1,12 @@
---> halve [1, 2, 3, 4]
+-- > halve [1, 2, 3, 4]
 --([1,2],[3,4])
 
 halve :: [a] -> ([a], [a])
 halve xs |even (length xs) = splitAt num xs
       where num = div (length xs) 2
+      
+-- safetail 1      
+safetail :: [a] -> [a]
+safetail xs = if null xs then [] else tail xs
+
+-- safetail 2
