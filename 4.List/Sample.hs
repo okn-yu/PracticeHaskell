29080cf4e7119ee2441c20e160a096ab662b1d03
okn-yu <37620738+okn-yu@users.noreply.github.com>
2018-12-30 18:34:53 +0900

    Update Sample.hs

--- 4.List/Sample.hs
+++ 4.List/Sample.hs
@@ -12,3 +12,8 @@ phyts :: Int -> [(Int, Int, Int)]
 phyts n = [(x, y, z)| x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
 
 -- 4.完全数の生成
+factors :: Int -> [Int]
+factors n = [x| x <- [1..n], (n `mod` x) == 0]
+
+perfect :: Int -> [Int]
+perfect n = [x| x <- [1..n], x == sum (factors x) - x]
