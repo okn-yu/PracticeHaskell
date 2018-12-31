11d26b582d76b8034300d7d52ce34c2aeb13e910
okn-yu <37620738+okn-yu@users.noreply.github.com>
2018-12-31 23:33:49 +0900

    Update Sample.hs

--- 5.RecursiveCall/Sample.hs
+++ 5.RecursiveCall/Sample.hs
@@ -1,9 +1,11 @@
+-- プログラミングHaskellの第5章練習問題の解答
+
 -- 1.累乗演算子の実装
 power :: Int -> Int -> Int
 power n m | m == 0 = n
           | n > 0  = n + power n (m - 1)
 
--- 2.and関数の実測
+-- 3-1.and関数の実測
 and' :: [Bool] -> Bool
 and' (False: _) = False
 and' (True: xs) = and' xs
