787d25c2690acfa4d5820719114ab711a91a72ff
okn-yu <37620738+okn-yu@users.noreply.github.com>
2018-12-31 22:12:51 +0900

    Update Sample.hs

--- 4.List/Sample.hs
+++ 4.List/Sample.hs
@@ -34,3 +34,17 @@ positions x xs = find x [(x', i)| (x', i) <- zip xs [0..n]]
 -- 7.リストの内積を計算する関数の実装
 scalarproduct :: [Int] -> [Int] -> Int
 scalarproduct x y = sum [x' * y'| (x', y') <- zip x y]
+
+-- 8.シーザー暗号の実装
+import Data.Char
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char
+int2let n =  chr(ord 'a' + n)
+
+shift :: Int -> Char -> Char
+shift n c | isLower c = int2let((let2int c + n) `mod` 26)
+          | isUpper c = int2let((let2int c + n) `mod` 26)
+          | otherwise = c
