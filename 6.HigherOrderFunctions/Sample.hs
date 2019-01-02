89c1392464e40486eb3b738f2514f5899ae1afd1
okn-yu <37620738+okn-yu@users.noreply.github.com>
2019-01-02 22:44:22 +0900

    Update Sample.hs

--- 6.HigherOrderFunctions/Sample.hs
+++ 6.HigherOrderFunctions/Sample.hs
@@ -1,4 +1,12 @@
 -- プログラミングHaskellの第7章練習問題の抜粋
 
--- 1.リスト内包表記 [f x | x <- xs, p x] を高階関数 map と filter を使って書き直すとどうなるか？
+-- 1.リスト内包表記 [f x | x <- xs, p x] を高階関数 map と filter を用いて実装
 map' f (filter p xs)
+
+-- 2-1.all関数の実装
+all' :: (a -> Bool) -> [a] -> Bool
+all' p [] = True
+all' p (x: xs) |p x = all' p xs
+               |otherwise = False
+               
+-- 2-2.any関数の実装               
