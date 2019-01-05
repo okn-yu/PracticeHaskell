44e9f051d0f9a9029f421fe37841d66983c89c3f
okn-yu <37620738+okn-yu@users.noreply.github.com>
2019-01-05 22:31:28 +0900

    Update Sample.hs

--- 6.HigherOrderFunctions/Sample.hs
+++ 6.HigherOrderFunctions/Sample.hs
@@ -1,27 +1,27 @@
 -- プログラミングHaskellの第7章練習問題の抜粋
 
--- 1.リスト内包表記 [f x | x <- xs, p x] を高階関数 map と filter を用いて実装
+-- 1. リスト内包表記 [f x | x <- xs, p x] を高階関数 map と filter を用いて実装
 map' f (filter p xs)
 
--- 2-1.all関数の実装
+-- 2-1. all関数の実装
 all' :: (a -> Bool) -> [a] -> Bool
 all' p [] = True
 all' p (x: xs) |p x = all' p xs
                |otherwise = False
                
--- 2-2.any関数の実装               
+-- 2-2. any関数の実装               
 any' :: (a -> Bool) -> [a] -> Bool
 any' p [] = False
 any' p (x: xs) |p x = True
                |otherwise = any' p xs
                
--- 2-3.takeWhile関数の実装
+-- 2-3. takeWhile関数の実装
 takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' p [] = []
 takeWhile' p (x: xs) | p x = x : takeWhile' p xs
                      | otherwise = []
 
--- 2-3.dropWhile関数の実装
+-- 2-3. dropWhile関数の実装
 dropWhile'  :: (a -> Bool) -> [a] -> [a]
 dropWhile' p [] = []
 dropWhile' p (x: xs) | p x = []
@@ -32,7 +32,9 @@ map' :: (a -> b) -> [a] -> [b]
 map' f [] = []
 map' f xs = foldr ((:) . f) [] xs
 
--- 3-1(2).filter関数のfoldrを用いた実装
+-- 3-1(2). filter関数のfoldrを用いた実装
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' f [] = []
 filter' f xs = foldr (\x xs -> if f x then x:xs else xs) [] xs
+
+-- 4. foldl を用いた十進表記を整数に変換する関数の実装
