7b50c24ba5d9063cf27eddf118bb123442b5ec70
okn-yu <37620738+okn-yu@users.noreply.github.com>
2019-01-26 14:53:50 +0900

    Update Sample.hs

--- Doc/7.Parser/Sample.hs
+++ Doc/7.Parser/Sample.hs
@@ -1,7 +1,17 @@
--- プログラミングHaskell練習問題8.10
+-- プログラミングHaskell練習問題8.10.1
 -- 整数のパーサーの実装
 integer :: Parser Int
 integer = do symbol "-"
              e <- natural
              return (-e)
              +++ natural
+             
+-- プログラミングHaskell練習問題8.10.2
+-- 普通のコメントのパーサーの実装
+comment :: Parser ()
+comment = do symbol "--"
+             many notEOL
+             char '\n'
+             return ()
+             +++ return ()
+          where notEOL = sat (/='\n')
